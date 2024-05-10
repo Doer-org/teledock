601fb61628fe8b56e34aaf12df71ae504e0a9dd0
seipan <yamasakipann0218@gmail.com>
2024-05-11 03:38:01 +0900

    fix : default backend url

--- internal/api/value.go
+++ internal/api/value.go
@@ -1,7 +1,7 @@
 package api
 
 var (
-	BackendURL = "https://ketos-server.doer-app.com"
+	BackendURL = "http://localhost:8000"
 	FrontURL   = "https://ketos.doer-app.com"
 	filePath   = "./tmp-tar/ketos-tmp-image.tar.gz"
 )
